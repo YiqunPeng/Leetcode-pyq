dfc7c6d0b3a1c6ae64f8d444c25c4f6a43aa1349
Yiqun Peng <ypeng1@weh5201-b4.andrew.cmu.edu>
2018-06-09 17:23:27 -0400

    ADD: 1TwoSum

--- solutions/1TwoSum.py
+++ solutions/1TwoSum.py
@@ -1,10 +1,10 @@
 class Solution():
     def twoSum(self, nums, target):
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+            :type nums: List[int]
+            :type target: int
+            :rtype: List[int]
+            """
         dic = {}
         for i in range(len(nums)):
             sub = target - nums[i]
