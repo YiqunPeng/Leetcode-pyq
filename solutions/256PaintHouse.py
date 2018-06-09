fbd02dfe871fa44dacff03ca749351c770a17034
Yiqun Peng <ypeng1@andrew.cmu.edu>
2018-06-09 14:35:05 -0400

    Update 256PaintHouse.py

--- solutions/256PaintHouse.py
+++ solutions/256PaintHouse.py
@@ -9,15 +9,15 @@ class Solution:
         house_num = len(costs)
         color_num = 3
         
-        dp = [[0] * 3 for i in range(house_num)]
+        dp = [[0 for j in range(color_num)] for i in range(2)]
         dp[0][0] = costs[0][0]
         dp[0][1] = costs[0][1]
         dp[0][2] = costs[0][2]
         
         for i in range(1, house_num):
-            dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + costs[i][0]
-            dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + costs[i][1]
-            dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + costs[i][2]
+            dp[i%2][0] = min(dp[1-i%2][1], dp[1-i%2][2]) + costs[i][0]
+            dp[i%2][1] = min(dp[1-i%2][0], dp[1-i%2][2]) + costs[i][1]
+            dp[i%2][2] = min(dp[1-i%2][0], dp[1-i%2][1]) + costs[i][2]
         
-        return min(dp[house_num-1])
+        return min(dp[1-house_num%2])
         
