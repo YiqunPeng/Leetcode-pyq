f345ac97b37b296399cb135a37164145ab097be8
Yiqun Peng <ypeng1@andrew.cmu.edu>
2018-06-09 15:17:08 -0400

    Update 216CombinationSumIII.py

--- solutions/216CombinationSumIII.py
+++ solutions/216CombinationSumIII.py
@@ -7,18 +7,15 @@ class Solution:
         """
         def backtracking(ans, k, n, cur, cur_sum, cur_len, s, e):
             if cur_sum == n and cur_len == k:
-                ans.append(copy.deepcopy(cur))
+                ans.append(cur)
                 return 0
             if cur_len > k or cur_sum > n:
                 return -1
             
             for i in range(s, e):
-                cur.append(i)
-                res = backtracking(ans, k, n, cur, cur_sum+i, cur_len+1, i+1, e)
-                cur.pop(-1)
+                res = backtracking(ans, k, n, cur+[i], cur_sum+i, cur_len+1, i+1, e)
                 if res == -1 or res == 0:
                     break
-        
         
         ans = []
         backtracking(ans, k, n, [], 0, 0, 1, 10)
