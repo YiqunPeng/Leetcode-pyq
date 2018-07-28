6b4f80b55821000bfd08b4a9dbca588b7a59578e
Yiqun Peng <ypeng1@andrew.cmu.edu>
2018-07-28 14:45:26 -0400

    Update 76MinimumWindowSubstring.py

--- solutions/76MinimumWindowSubstring.py
+++ solutions/76MinimumWindowSubstring.py
@@ -1,64 +1,37 @@
 class Solution:
+    # hash table + two pointer
+    # time: O(n)
+    # space: O(k) k == len(t)
     def minWindow(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: str
         """
-        def contain_all(s_d, t_d):
-            for key in t_d:
-                if key not in s_d or t_d[key] > s_d[key]:
-                    return False
-            return True
-            
-    
         if not s: return ''
         if not t: return s[0]
-    
-        t_dic = {}
-        for i in t:
-            t_dic[i] = t_dic.get(i, 0) + 1
         
-        s_dic = {}
-        pos = 0
-        left, right = -1, -1
-        while pos < len(s):
-            if s[pos] not in t_dic:
-                pos += 1
-                continue
-            else:
-                if left == -1: left = pos
-                s_dic[s[pos]] = s_dic.get(s[pos], 0) + 1
-                while s[left] not in t_dic or s_dic[s[left]] > t_dic[s[left]]:
-                    if s[left] in t_dic:
-                        s_dic[s[left]] -= 1
-                    left += 1
-                if contain_all(s_dic, t_dic):
-                    pos += 1
-                    break
-                pos += 1
-
-        right = pos
-        if not contain_all(s_dic, t_dic): return ''
-        ans = s[left:right]
-        if right == len(s): return ans
+        ans = [0, len(s)-1]      
+        t_dict = collections.defaultdict(int)
+        for c in t:
+            t_dict[c] += 1
+        s_dict = collections.defaultdict(int)
         
+        left, right = 0, 0
+        cover_flag = False
         while right < len(s):
-            if s[right] not in t_dic:
-                right += 1
-                continue
-            else:
-                if s[right] == s[left]:
-                    s_dic[s[right]] += 1
-                    while s[left] not in t_dic or s_dic[s[left]] > t_dic[s[left]]:
-                        if s[left] in t_dic:
-                            s_dic[s[left]] -= 1
-                        left += 1
-                    if right - left + 1 < len(ans):
-                        ans = s[left:right+1]
-                        if len(ans) == len(t): return ans            
-                else:
-                    s_dic[s[right]] += 1
-                right += 1
+            if s[right] in t_dict:
+                s_dict[s[right]] += 1
+                while s[left] not in t_dict or s_dict[s[left]] > t_dict[s[left]]:
+                    if s[left] in t_dict: s_dict[s[left]] -= 1
+                    left += 1
+                if not cover_flag:
+                    cover_flag = all([s_dict[key] >= t_dict[key] for key in t_dict])
+                if cover_flag:
+                    if right - left < ans[1] - ans[0]:
+                        ans[0], ans[1] = left, right
+                    if ans[1] - ans[0] + 1 == len(t): return s[ans[0]:ans[1]+1]
+            right += 1
         
-        return ans
+        return s[ans[0]:ans[1]+1] if cover_flag else ''
+    
