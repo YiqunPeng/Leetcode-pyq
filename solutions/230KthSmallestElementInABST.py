bdc25e6957305a6ea66a80d137a7192581f6583b
Yiqun Peng <ypeng1@andrew.cmu.edu>
2018-08-04 14:49:29 -0400

    Create 230KthSmallestElementInABST.py

---
+++ solutions/230KthSmallestElementInABST.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def kthSmallest(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: int
+        """
+        stack = []
+        
+        while root or stack:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                k -= 1
+                if k == 0:
+                    return root.val
+                root = root.right
