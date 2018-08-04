0a31ce3fab44a1f7e3c016fd11d34626f3a84476
Yiqun Peng <ypeng1@andrew.cmu.edu>
2018-08-04 14:18:56 -0400

    Update 110BalancedBinaryTree.py

--- solutions/110BalancedBinaryTree.py
+++ solutions/110BalancedBinaryTree.py
@@ -1,37 +1,28 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
+class Solution(object):
     def isBalanced(self, root):
         """
         :type root: TreeNode
         :rtype: bool
         """
-        def depth(r):
-            if not r:
-                return 0
-            if not r.left and not r.right:
-                return 1
-            return max(depth(r.left), depth(r.right)) + 1
-        
-        
-        def balanced_tree(r):
-            if not r or (not r.left and not r.right):
-                return True
-            left, right = 0, 0
-            if r.right:
-                right = depth(r.right)
-            if r.left:
-                left = depth(r.left)
-            if abs(right-left) > 1:
-                return False
+        def dfs(root):
+            if not root: return 0
+            if not root.left and not root.right: return 1
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+            if left == -1 or right == -1:
+                return -1
+            
+            if abs(left - right) <= 1:
+                return max(left, right) + 1
             else:
-                return balanced_tree(r.left) and balanced_tree(r.right) 
-        
-        
-        if not root: return True        
-        return balanced_tree(root)
+                return -1
+
+        return dfs(root) != -1
