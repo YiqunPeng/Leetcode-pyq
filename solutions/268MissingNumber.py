5e83ff3be78ec8b478d6395afbebf82e21c9f002
Yiqun Peng <ypeng1@andrew.cmu.edu>
2018-08-13 14:43:28 -0400

    Update 268MissingNumber.py

--- solutions/268MissingNumber.py
+++ solutions/268MissingNumber.py
@@ -1,7 +1,31 @@
-class Solution(object):
+class Solution:
+    # swap, two pass
+    # time: O(n)
+    # space: O(1)
     def missingNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        return sum(range(len(nums)+1)) - sum(nums)
+        n = len(nums)
+        
+        for i in range(n):
+            while nums[i] != n and nums[i] != i:
+                a = nums[i]
+                nums[i], nums[a] = nums[a], nums[i]
+        
+        for i in range(n):
+            if nums[i] != i:
+                return i
+        return n
+
+    # math
+    # time: O(n)
+    # space: O(1)
+    # def missingNumber(self, nums):
+    #     """
+    #     :type nums: List[int]
+    #     :rtype: int
+    #     """
+    #     n = len(nums)
+    #     return n * (n + 1) // 2 - sum(nums)
