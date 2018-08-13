6001feb3fed04497cfe85468a4800d46c4fba2cd
Yiqun Peng <ypeng1@andrew.cmu.edu>
2018-08-13 16:57:26 -0400

    Create 518CoinChangeII.py

---
+++ solutions/518CoinChangeII.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+
+        for c in coins:
+            if c < amount + 1:
+                dp[c] += 1
+            for i in range(c + 1, amount + 1):
+                dp[i] += dp[i - c]
+        
+        return dp[-1]
