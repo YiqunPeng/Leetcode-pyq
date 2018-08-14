c986d24d6cbe51557b215db6e2a12dfbdfd2d112
YiqunPeng <ypeng1@andrew.cmu.edu>
2018-08-14 12:27:51 -0400

    0814

---
+++ solutions/483SmallestGoodBase.py
@@ -0,0 +1,15 @@
+class Solution:
+    def smallestGoodBase(self, n):
+        """
+        :type n: str
+        :rtype: str
+        """
+        n = int(n)
+        max_m = int(math.log(n, 2))
+        
+        for m in range(max_m, 1, -1):
+            k = int(n ** (m ** -1))
+            if (k ** (m + 1) - 1) // (k - 1) == n:
+                return str(k)
+        
+        return str(n - 1)
