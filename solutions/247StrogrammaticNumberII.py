eb92ba9499d323ffedf24d5f3be7a112c07cd1b5
Yiqun Peng <ypeng1@andrew.cmu.edu>
2018-08-27 12:53:38 -0400

    Update 247StrogrammaticNumberII.py

--- solutions/247StrogrammaticNumberII.py
+++ solutions/247StrogrammaticNumberII.py
@@ -4,37 +4,20 @@ class Solution:
         :type n: int
         :rtype: List[str]
         """
-        def rotated_string(s):
-            dic = {'0': '0', '1': '1', '6' : '9', '8': '8', '9': '6'}
-            res = ''
-            for i in range(len(s)-1, -1, -1):
-                res = res + dic[s[i]]
+        def dfs(length, remain):
+            if remain == 0: return ['']
+            if remain == 1: return ['0', '1', '8']
+
+            sub_res = dfs(length, remain - 2)
+
+            res = []
+            for num in sub_res:
+                if length != remain: res.append('0' + num + '0')
+                res.append('1' + num + '1')
+                res.append('6' + num + '9')
+                res.append('8' + num + '8')
+                res.append('9' + num + '6')
+
             return res
-                
         
-        def backtracking(ans, cur, cur_len, half_len, stro_nums):
-            if cur_len == half_len:
-                ans.append(cur)
-                return
-            for i in range(len(stro_nums)):
-                if cur_len == 0 and stro_nums[i] == '0': continue
-                backtracking(ans, cur+stro_nums[i], cur_len+1, half_len, stro_nums)
-         
-        if n == 0: return []
-            
-        stro_nums = ['0', '1', '6', '8', '9']
-        single_stro_nums = ['0', '1', '8']
-        
-        ans = []
-        backtracking(ans, '', 0, n//2, stro_nums)
-        
-        temp = []
-        if n % 2 == 0:
-            for a in ans:
-                temp.append(a + rotated_string(a))
-        else:
-            for a in ans:
-                for num in single_stro_nums:
-                    temp.append(a + num + rotated_string(a))
-        
-        return temp
+        return dfs(n, n)
